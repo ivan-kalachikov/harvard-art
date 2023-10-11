0ba5745526f4b533b75798a1787b9099b6de4b93
Ivan Kalachikov <ki@megaperfume.ru>
2023-10-11 21:42:52 +0800

    add and set up a lightbox

--- app/[slug]/page.tsx
+++ app/[slug]/page.tsx
@@ -3,10 +3,21 @@
 import { useQuery, UseQueryResult } from '@tanstack/react-query';
 import { ArtObjectDetails } from '@/types/object';
 import { getObjectDetails } from '@/api';
+import Lightbox from 'yet-another-react-lightbox';
+import { Captions, Counter, Zoom } from 'yet-another-react-lightbox/plugins';
+import { useState, useRef } from 'react';
+import Link from 'next/link';
 import Image from 'next/image';
 import styles from './page.module.scss';
+import 'yet-another-react-lightbox/styles.css';
+import 'yet-another-react-lightbox/plugins/captions.css';
+import 'yet-another-react-lightbox/plugins/counter.css';
 
 export default function Details({ params }: { params: { slug: string } }) {
+  const [openLightbox, setOpenLightbox] = useState(false);
+  const [lightboxImageIndex, setLightboxImageIndex] = useState(0);
+  const captionsRef = useRef(null);
+
   const {
     // isLoading,
     data, // error,
@@ -22,11 +33,42 @@ export default function Details({ params }: { params: { slug: string } }) {
     },
   );
 
+  const handleImageClick = (index: number) => {
+    setOpenLightbox(true);
+    setLightboxImageIndex(index);
+  };
+
   return (
     <div className='object-details'>
       <h1>{data?.title}</h1>
       <p>{data?.description}</p>
-      {data?.images.map((image) => (
+      <Link href='/'>Back</Link>
+      <Lightbox
+        plugins={[Captions, Counter, Zoom]}
+        captions={{ ref: captionsRef }}
+        open={openLightbox}
+        close={() => setOpenLightbox(false)}
+        counter={{ container: { style: { top: '30px' } } }}
+        slides={data?.images.map((image) => ({
+          src: image.baseimageurl,
+          width: image.width,
+          height: image.height,
+          alt: image.publiccaption,
+          title: image.publiccaption,
+        }))}
+        index={lightboxImageIndex}
+        on={{
+          click: () => {
+            (captionsRef.current?.visible
+              ? captionsRef.current?.hide
+              : captionsRef.current?.show)?.();
+          },
+        }}
+        zoom={{
+          scrollToZoom: true,
+        }}
+      />
+      {data?.images.map((image, index) => (
         <div className={styles.image}>
           <Image
             key={image.imageid}
@@ -34,6 +76,8 @@ export default function Details({ params }: { params: { slug: string } }) {
             src={image.baseimageurl}
             alt={image.copyright}
             sizes='(300px)'
+            alt={image.publiccaption}
+            onClick={() => handleImageClick(index)}
           ></Image>
         </div>
       ))}
